7d9a0979fa90fd2f950a67613b3465e81504cc50
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-16 02:39:46 +0300

    #1 Hello world implemented

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Add app: <App />
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
 registerServiceWorker();
