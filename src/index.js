c67a5c7c6a98c34a381a3ec5f941b1a9310c6796
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-16 02:56:56 +0300

    #2 Introducing JSX completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-// Add app: <App />
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
-registerServiceWorker();
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+let user = {
+  firstName: 'Nacho',
+  lastName: 'Tsvetkov'
+};
+
+// user = null;
+
+function getGreeting() {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+
+  return <h1>Hello, Stranger.</h1>;
+}
+
+let element = React.createElement(
+  'h1',
+  { className: 'greeting' },
+  'Hello, world!'
+);
+element = getGreeting();
+
+ReactDOM.render(element, document.getElementById('root'));
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// );
+// registerServiceWorker();
