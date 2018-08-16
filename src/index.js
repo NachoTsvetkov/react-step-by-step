3edd79b6e4666269a4490ae5f5c8e7b5baceb97e
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-16 13:08:11 +0300

    #5 state and lifecycle implemented

--- src/index.js
+++ src/index.js
@@ -2,69 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function WelcomeFunctional(props) {
-  return <h1>Hello, {props.name}</h1>;
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}</h2>;
 }
 
-class Welcome extends React.Component {
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
   }
 }
 
 function App(props) {
   return (
     <div>
-      <WelcomeFunctional name="Nacho" />
-      <Welcome name="Marty" />
-      <WelcomeFunctional name="Sara" />
+      <Clock />
+      <Clock />
+      <Clock />
     </div>
   );
 }
 
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64'
-  }
-};
-
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-  return (
-    <img
-      className="Avatar"
-      src={props.author.avatarUrl}
-      alt={props.author.name}
-    />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar author={props.author} />
-      <div className="UserInfo-name">{props.author.name}</div>
-    </div>
-  );
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo author={props.author} />
-      <div className="Comment-text">{props.text}</div>
-      <div className="Comment-date">{formatDate(props.date)}</div>
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <Comment date={comment.date} text={comment.text} author={comment.author} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
