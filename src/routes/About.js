9b16011bab924c5ab1e9d189771f86e1cbfd60bc
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-17 07:18:11 +0300

    rendering added

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export function About(props) {
-  return <div />;
+  return <div>About</div>;
 }
+
+ReactDOM.render(<About />, document.getElementById('root'));
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export function Home(props) {
-  return <div />;
+  return <div>Home</div>;
 }
+
+ReactDOM.render(<Home />, document.getElementById('root'));
