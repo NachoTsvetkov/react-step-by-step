ab551d7354381511d84488f9f0bde00f59b28560
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-19 16:20:48 +0300

    #18 fragments implemented

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const FancyButton = React.forwardRef((props, ref) => (
-  <button ref={ref} className="FancyButton">
-    {props.children}
-  </button>
-));
+class Columns extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <td>Hello</td>
+        <td>World</td>
+      </React.Fragment>
+    );
+  }
+}
 
-// You can now get a ref directly to the DOM button:
-const ref = React.createRef();
+class Table extends React.Component {
+  render() {
+    return (
+      <table>
+        <tr>
+          <Columns />
+        </tr>
+      </table>
+    );
+  }
+}
 
-ReactDOM.render(
-  <FancyButton ref={ref}>Click me!</FancyButton>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Table />, document.getElementById('root'));
