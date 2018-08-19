ab35c67078a9b5d4dc8b4f03ef23afdbfa23ae5d
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-19 17:01:44 +0300

    #21 jsx in depth implemented

--- src/index.js
+++ src/index.js
@@ -2,77 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class DataSource {
-  static getComments() {
-    return 'Comment Data';
-  }
+const Button = props => {
+  const { kind, ...other } = props;
+  const className = kind === 'primary' ? 'PrimaryButton' : 'SecondaryButton';
+  return <button className={className} {...other} />;
+};
 
-  static getBlogPost(id) {
-    return 'Blog Data';
-  }
-
-  static addChangeListener(onChange) {}
-
-  static removeChangeListener(onChange) {}
-}
-
-// This function takes a component...
-function withSubscription(WrappedComponent, selectData) {
-  // ...and returns another component...
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleChange = this.handleChange.bind(this);
-      this.state = {
-        data: selectData(DataSource, props)
-      };
-    }
-
-    componentDidMount() {
-      // ... that takes care of the subscription...
-      DataSource.addChangeListener(this.handleChange);
-    }
-
-    componentWillUnmount() {
-      DataSource.removeChangeListener(this.handleChange);
-    }
-
-    handleChange() {
-      this.setState({
-        data: selectData(DataSource, this.props)
-      });
-    }
-
-    render() {
-      // ... and renders the wrapped component with the fresh data!
-      // Notice that we pass through any additional props
-      return <WrappedComponent data={this.state.data} {...this.props} />;
-    }
-  };
-}
-
-function CommentList(props) {
-  return <div>{props.data}</div>;
-}
-
-const CommentListWithSubscription = withSubscription(CommentList, DataSource =>
-  DataSource.getComments()
-);
-
-function BlogPost(props) {
-  return <div>{props.data}</div>;
-}
-
-const BlogPostWithSubscription = withSubscription(
-  BlogPost,
-  (DataSource, props) => DataSource.getBlogPost(props.id)
-);
+const App = () => {
+  return (
+    <div>
+      <Button kind="primary" onClick={() => console.log('clicked!')}>
+        Hello World!
+      </Button>
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <div>
-    <CommentListWithSubscription />
-    <BlogPostWithSubscription />
-  </div>,
+  <App />,
 
   document.getElementById('root')
 );
