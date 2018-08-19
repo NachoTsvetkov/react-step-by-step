f87f23a3605f773719ba06dd32333836b2cb2c0e
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-19 16:45:13 +0300

    #19 higher order components implemented

--- src/index.js
+++ src/index.js
@@ -2,27 +2,77 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Columns extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <td>Hello</td>
-        <td>World</td>
-      </React.Fragment>
-    );
+class DataSource {
+  static getComments() {
+    return 'Comment Data';
   }
+
+  static getBlogPost(id) {
+    return 'Blog Data';
+  }
+
+  static addChangeListener(onChange) {}
+
+  static removeChangeListener(onChange) {}
 }
 
-class Table extends React.Component {
-  render() {
-    return (
-      <table>
-        <tr>
-          <Columns />
-        </tr>
-      </table>
-    );
-  }
+// This function takes a component...
+function withSubscription(WrappedComponent, selectData) {
+  // ...and returns another component...
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {
+        data: selectData(DataSource, props)
+      };
+    }
+
+    componentDidMount() {
+      // ... that takes care of the subscription...
+      DataSource.addChangeListener(this.handleChange);
+    }
+
+    componentWillUnmount() {
+      DataSource.removeChangeListener(this.handleChange);
+    }
+
+    handleChange() {
+      this.setState({
+        data: selectData(DataSource, this.props)
+      });
+    }
+
+    render() {
+      // ... and renders the wrapped component with the fresh data!
+      // Notice that we pass through any additional props
+      return <WrappedComponent data={this.state.data} {...this.props} />;
+    }
+  };
 }
 
-ReactDOM.render(<Table />, document.getElementById('root'));
+function CommentList(props) {
+  return <div>{props.data}</div>;
+}
+
+const CommentListWithSubscription = withSubscription(CommentList, DataSource =>
+  DataSource.getComments()
+);
+
+function BlogPost(props) {
+  return <div>{props.data}</div>;
+}
+
+const BlogPostWithSubscription = withSubscription(
+  BlogPost,
+  (DataSource, props) => DataSource.getBlogPost(props.id)
+);
+
+ReactDOM.render(
+  <div>
+    <CommentListWithSubscription />
+    <BlogPostWithSubscription />
+  </div>,
+
+  document.getElementById('root')
+);
